c6b1d0294a09778d93139d5969dc9ea9542ab8b7
Sarah <s.bigot4982@gmail.com>
2025-04-09 13:08:59 +0200

    funciona readIdea, createIdea y ver likes pero no se ve el ultimo like

--- data_base.js
+++ data_base.js
@@ -12,6 +12,28 @@ function conect(){
     });
 }
 
+
+export function checkUser(user_name,userpassword){
+    return new Promise((fulfill,reject) => {
+
+        const conexion = conect();
+
+        conexion`SELECT * FROM users WHERE user_name = ${user_name}`
+        .then( user => {
+            conexion.end();
+
+            if(user.length == 0){
+                return reject({ error : "user not found in the database" })
+            }
+            return user[0].user_password == userpassword ? fulfill(user) : reject({ error : "password doesn't match with user" })
+        })
+        .catch( error => {
+            conexion.end();
+            reject({ error : "data base error" });
+        });
+    });
+}
+
 export function readIdeas(){
     return new Promise((fulfill,reject) => {
 
@@ -38,9 +60,9 @@ export function createIdea(file_name,idea_name){
         let url = `/upload/img_ideas/${file_name}`
         
         conexion`INSERT INTO ideas (url,file_name,idea_name) VALUES (${url},${file_name},${idea_name}) RETURNING id`
-        .then( ([{id}]) => {
+        .then( ([{id,url}]) => {
             conexion.end();
-            fulfill(id);
+            fulfill(id,url);
         })
         .catch( error => {
             conexion.end();
@@ -63,5 +85,58 @@ export function deleteIdea(id){
             console.log("aqui", error) 
             reject({ error : "data base error 1" }); 
         });
+    });
+}
+
+export function addLike({likes,user}){
+    return new Promise((fulfill,reject) => {
+
+        const conexion = conect();
+        console.log("likes",likes)
+
+        let idLikes = likes.map(like => like.id)
+        let urlLikes = likes.map(like => like.url)
+        console.log("aqui likes y user",likes,user)
+        
+        conexion`UPDATE users SET liked_id = ${idLikes} WHERE user_name = ${user} RETURNING liked_id`
+        .then( ([{liked_id}]) => {
+            conexion.end();
+            
+            fulfill({liked_id,likes});
+        })
+        .catch( error => {
+            conexion.end();
+
+            reject({ error : "data base error" });
+        });
+
+    });
+}
+
+export function readLikes({user}){
+    return new Promise((fulfill,reject) => {
+
+        const conexion = conect();
+
+
+
+
+        
+        // selecting two columns in two different tables, unnesting values from liked_id's array and rename the individual values, then joining the id with the matching url
+        conexion`SELECT unnest_liked_id.liked_id, ideas.url
+                    FROM users 
+                    JOIN LATERAL unnest(users.liked_id) AS unnest_liked_id(liked_id) ON TRUE 
+                    JOIN ideas ON ideas.id = unnest_liked_id.liked_id 
+                    WHERE users.user_name = ${user}`
+        .then( likes => {
+            conexion.end();
+            console.log("estos son los likes de la funcion readLikes: ",likes)
+            fulfill(likes);
+        })
+        .catch( error => {
+            conexion.end();
+            reject({ error : "data base error 1" });
+        });
+        
     });
 }
--- index.js
+++ index.js
@@ -16,26 +16,54 @@ server.use(cors());
 import multer from 'multer'
 const storage = multer.diskStorage({
     destination: "./upload/img_ideas",
+    
     filename: (request, file, callback) => {
         const newFileName = Math.floor(Math.random() * 801) * Math.floor(Math.random() * 742) + Math.floor(Math.random() * 218) ;
         const ext = path.extname(file.originalname);
         callback(null, newFileName + ext);
+        
       }
+      
 });
 const upload = multer({ storage : storage })
 
 
 
-import { readIdeas,createIdea,deleteIdea } from './data_base.js'
+import { readIdeas,createIdea,deleteIdea,checkUser,addLike,readLikes } from './data_base.js'
 
 
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-
+/*
 if(process.env.TESTS){
     server.use("/tests",express.static("./tests")); 
 }
+*/
+
+server.post("/home", async (request,response,next) => {
+    try{
+       
+        let user_name = request.body.username;
+        let userpassword = request.body.password;
+
+        let match = await checkUser(user_name,userpassword);
+        
+        if(match){
+            
+            response.status(200);
+
+            return response.send(match)
+        }
+        next()
+
+       
+    }catch(error){
+        response.status(500);
+
+        response.json({ error : error });
+    }
+})
 
 
 server.get("/ideas", async (request,response) => {
@@ -52,21 +80,24 @@ server.get("/ideas", async (request,response) => {
     }
 });
 
+server.get("/idea/:id", (request,response) => {
+    
+})
 
 server.post("/upload/img_ideas", upload.single("img"), async (request,response) => {
     try{
-       
+        console.log("contenido del request",request)
+        console.log("contenido del request.file",request.file)
         const idea_name = request.body.filename;
         const file_name = request.file.filename;
         const extension = path.extname(request.file.originalname);
-        const newFileName = `${file_name}${extension}`;
         /*
         response.json({ 
             url : `/upload/img_ideas/${newFileName}`,
             fileName :  file_name,
             ideaName : idea_name
         });
-*/
+*/      
         let id = await createIdea(file_name,idea_name);
         console.log("este es el id", id)
         return response.json({id});
@@ -74,7 +105,7 @@ server.post("/upload/img_ideas", upload.single("img"), async (request,response) 
        
     }catch(error){
         response.status(500);
-
+        console.log("aqui el error 500 del middleware upload img",error)
         response.json({ error : "server error"});
     }
     
@@ -88,16 +119,8 @@ server.delete("/idea/delete/:id", async (request,response,next) => {
 
         let deletedIdea = await deleteIdea(request.params.id);
 
-
         if(deletedIdea == 1){
             response.status(204);
-
-            unlink("../upload/img_ideas/:id", (error) => {
-                if(error){
-                    return console.log("ha habido un error",error)
-                }
-                console.log("se ha verificado el archivo")
-            })
 
             return response.send("");
         }
@@ -128,5 +151,65 @@ server.get("/upload/img_ideas", async (req,res) => {
     }  
 })
 */
+
+server.post("/add-like", async (request,response) => {
+
+    let {likes,user} = request.body
+
+    try{
+        
+        let id = await addLike({likes,user});
+
+        return response.json({likes});
+
+    }catch(error){
+
+        response.status(500);
+
+        response.json({ error : error })
+
+    }
+    
+})
+
+server.post("/likes", async (request,response) => {
+
+
+
+
+    
+    console.log("request.body es igual a",request.body)
+    let {user} = request.body
+    
+    try{
+        let likes = await readLikes({user});
+        console.log("respnse en index?js",request.body);
+        response.json({likes});
+
+    }catch(error){
+
+        response.status(500);
+
+        response.json({ error : error })
+    }
+    
+});
+
+
+server.use((error,request,response,next) => { 
+
+    response.status(400); 
+    console.log("middleware 400")
+
+    response.json({ error : "request error" });
+});
+
+server.use((request,response) => { 
+
+    response.status(404);
+    console.log("middleware 404")
+
+    response.send("404 not found");
+});
 
 server.listen(process.env.PORT);
